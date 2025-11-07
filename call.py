99c39a29f26eb8a64d090120fe9b2cea830f0da7
lylhs <lyl@impcas.ac.cn>
2025-11-07 09:58:43 +0800

    rename in call.py

--- call.py
+++ call.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     # for v in [4,8,16,32]:
     for v in [16,32]:
         for i in name:
-            cmd='screen -dmS shiyan{0}{1}{2} bash -c "python3 ./compute5.py -m {0} -n {1} -v {2}"'.format("SDSG",i,v)
+            cmd='screen -dmS shiyan{0}{1}{2} bash -c "python3 ./ModelDepath.py -m {0} -n {1} -v {2}"'.format("SDSG",i,v)
         
             while (True):
                 time.sleep(3)
@@ -48,49 +48,4 @@ if __name__ == "__main__":
 
 
             print(ret)
-    # name = ["Cite","Cora","Pub","Computer","Photo","Blog","Wiki","CS","Physics"] 
-    # for v in [4,8,16,32]:
-    #     for i in name:
-    #         cmd='screen -dmS shiyan{0}{1}{2} bash -c "python3 ./compute5.py -m {0} -n {1} -v {2}"'.format("SG",i,v)
-        
-    #         while (True):
-    #             count=get_gpu_process_count(0)
-    #             if count <1:
-    #                 ret=os.system(cmd)
-    #                 time.sleep(3)
-    #                 break    
-                
-    #             elif not (((i == "Physics") or (i=="CS")) &(v==32)) & count<2:
-    #                 ret=os.system(cmd)
-    #                 time.sleep(3)
-    #                 break
-    #             else:
-    #                 time.sleep(15)
-
-    #         print(ret)
-
-    
-
-    # name = ["Cite","Cora","Pub","Computer","Photo","Blog","CS","Physics","Wiki"] 
-    # # for v in [4,8,16,32]:
-    # v=4
-    # for i in name:
-    #     cmd='screen -dmS shiyan{0}{1}{2} bash -c "python3 ./compute5.py -m {0} -n {1} -v {2}"'.format("supGAT",i,v)
-    
-    #     while (True):
-    #         time.sleep(3)
-    #         count=get_gpu_process_count(0)
-    #         if count <1:
-    #             ret=os.system(cmd)
-    #             time.sleep(5)
-    #             break    
-            
-    #         elif not((i=="Physics") or (i=="CS")) and count <2:
-    #             ret=os.system(cmd)
-    #             time.sleep(5)
-    #             break
-    #         else:
-    #             time.sleep(15)
-
-
-    #     print(ret)
+ 
